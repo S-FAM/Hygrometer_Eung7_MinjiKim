c2b00aa40bd743ebaa1a860ab035dadf97fd0b11
Minji Kim <minji_0801@naver.com>
2022-07-03 21:13:20 +0900

    Initial Commit

---
+++ Hygrometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hygrometer
+//
+//  Created by 김민지 on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
