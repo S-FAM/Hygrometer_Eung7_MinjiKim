abf3d7fbca4b6df82cdb89d38dbedf695f14ab91
Minji Kim <minji_0801@naver.com>
2022-07-27 21:19:48 +0900

    Implement responsive UI of EntryView

--- Hygrometer/Entry/EntryViewController.swift
+++ Hygrometer/Entry/EntryViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 import WaterDrops
 
 class EntryViewController: UIViewController {
+
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.distribution = .fill
+    stackView.alignment = .center
+    stackView.spacing = 15
+
+    return stackView
+  }()
+
   private lazy var bearImage: UIImageView = {
     let imageView = UIImageView()
     imageView.image = UIImage(named: "bear")
@@ -21,17 +32,12 @@ class EntryViewController: UIViewController {
   ///   - Length: water drops movement range
   ///   - Duration: water drops movement speed
   private lazy var waterDrops = WaterDropsView(
-    frame: CGRect(
-      x: view.frame.minX + 175,
-      y: view.frame.minY + 185,
-      width: 20,
-      height: 100
-    ),
+    frame: stackView.frame,
     direction: .up,
-    dropNum: 3,
+    dropNum: 5,
     color: UIColor.white.withAlphaComponent(0.7),
-    minDropSize: 10,
-    maxDropSize: 20,
+    minDropSize: Measure.Entry.waterDropsMinSize,
+    maxDropSize: Measure.Entry.waterDropsMaxSize,
     minLength: 50,
     maxLength: 100,
     minDuration: 1,
@@ -40,7 +46,6 @@ class EntryViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setupUI()
-    setupAnimation()
   }
 
   override func viewDidAppear(_ animated: Bool) {
@@ -53,18 +58,30 @@ class EntryViewController: UIViewController {
   func setupUI() {
     view.backgroundColor = .gray
 
-    [bearImage, waterDrops].forEach {
-      view.addSubview($0)
+    view.addSubview(stackView)
+
+    stackView.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+    }
+
+    [waterDrops, bearImage].forEach {
+      stackView.addArrangedSubview($0)
     }
 
     bearImage.snp.makeConstraints { make in
-      make.center.equalToSuperview()
-      make.width.equalTo(190)
-      make.height.equalTo(219)
+      let width = Measure.Entry.bearImageWidth
+      let height = width * (1.1466)
+      make.width.equalTo(width); make.height.equalTo(height)
     }
-  }
 
-  func setupAnimation() {
     waterDrops.addAnimation()
+  }
+}
+
+extension Measure {
+  fileprivate struct Entry {
+    static let bearImageWidth: CGFloat = Measure(regular: 160, medium: 145, small: 125, tiny: 105).forScreen
+    static let waterDropsMinSize: CGFloat = Measure(regular: 10, medium: 10, small: 5, tiny: 5).forScreen
+    static let waterDropsMaxSize: CGFloat = Measure(regular: 20, medium: 20, small: 15, tiny: 15).forScreen
   }
 }
